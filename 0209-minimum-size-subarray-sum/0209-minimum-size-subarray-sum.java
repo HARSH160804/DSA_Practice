37e9b7e5eddcedfc5ed74676bd0aaaf766674093
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-07-11 04:01:34 +0530

    Time: 1 ms (99.95%), Space: 69 MB (36.32%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,5 +1,52 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+
+        int res = Integer.MAX_VALUE;
+        int low=0;
+        int high=0;
+        int length = nums.length;
+
+        int sum=0;
+
+        // made window
+
+        for(int i=low;i<=high;i++)
+        {
+            sum= sum+nums[i];
+
+        }  
+            
+            while(high<length)
+            {
+            //fire karo
+            if(sum>=target)
+            {
+                res = Math.min(res,(high-low+1));
+                sum=sum-nums[low];
+                low++;
+            }
+                 
+            // hire karte raho
+            else if(sum<target)
+            {
+                high++;
+                if(high<length){
+                sum=sum+nums[high];}
+            }
+            }
+        
+        // ternary    ()? _:_
+        
+    return (res == Integer.MAX_VALUE) ? 0:res;
+        
+    }
+}
+
+
+/*solution two
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
         
         int low=0;
         int high=0;
@@ -33,3 +80,5 @@ class Solution {
 
 // res  sum > = target
 //(min length) 
+
+*/
