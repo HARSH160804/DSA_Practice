57c22be50807d8eec91c7f6035e495c838405620
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-07-11 02:37:13 +0530

    Create maximum sum subarray.java

---
+++ 0209-minimum-size-subarray-sum/maximum sum subarray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int low=0;
+        int high=k-1;
+        int res = Integer.MIN_VALUE;
+        int sum =0;
+        int length=arr.length;
+        for(int i=0;i<k;i++)
+        {
+        sum = sum+arr[i];
+        }
+        
+        while(high<length)
+        {
+            res = Math.max(sum,res);
+            high++;
+            
+            if(high<length)
+            {
+                sum=sum+arr[high]-arr[low];
+                low++;
+            }
+        }
+        
+        
+        
+        return res;
+    }
+}
