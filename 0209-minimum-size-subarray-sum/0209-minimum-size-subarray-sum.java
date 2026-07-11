498e113270cff68d205bc4189dcdc96f9947f9ff
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-07-11 18:14:15 +0530

    Time: 1 ms (99.95%), Space: 69.2 MB (28.9%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,84 +1,26 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+        
 
         int res = Integer.MAX_VALUE;
-        int low=0;
-        int high=0;
         int length = nums.length;
-
-        int sum=0;
-
-        // made window
-
-        for(int i=low;i<=high;i++)
-        {
-            sum= sum+nums[i];
-
-        }  
-            
-            while(high<length)
-            {
-            //fire karo
-            if(sum>=target)
-            {
-                res = Math.min(res,(high-low+1));
-                sum=sum-nums[low];
-                low++;
-            }
-                 
-            // hire karte raho
-            else if(sum<target)
-            {
-                high++;
-                if(high<length){
-                sum=sum+nums[high];}
-            }
-            }
-        
-        // ternary    ()? _:_
-        
-    return (res == Integer.MAX_VALUE) ? 0:res;
-        
-    }
-}
-
-
-/*solution two
-
-class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        
         int low=0;
-        int high=0;
         int sum =0;
-        int len = nums.length;
-        int res = Integer.MAX_VALUE;
 
-        
-        while(high < len)
+        //hiring loop -> step 1 .
+        for(int high=0;high<length;high++)
         {
             sum = sum + nums[high];
-
-           
-            while( sum >= target)
+            
+            //condition and firing  {static mai if}
+            while(sum>=target)
             {
-                res = Math.min(res, (high-low+1));
-                sum = sum - nums[low];
+                res = Math.min(res,high-low+1);
+                sum = sum-nums[low];
                 low++;
-                
             }
-            high++;
+
         }
-        
-
-        return  (res == Integer.MAX_VALUE) ? 0:res;
-
+        return (res == Integer.MAX_VALUE)? 0:res;
     }
-
-    
 }
-
-// res  sum > = target
-//(min length) 
-
-*/
