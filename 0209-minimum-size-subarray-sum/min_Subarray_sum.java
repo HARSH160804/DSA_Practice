d84df994e86b291e1331d6d627c2b72530de0ce0
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-07-11 17:47:46 +0530

    Create min_Subarray_sum.java

---
+++ 0209-minimum-size-subarray-sum/min_Subarray_sum.java
@@ -0,0 +1,30 @@
+
+/// learn this template
+
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        
+
+        int res = Integer.MAX_VALUE;
+        int length = nums.length;
+        int low=0;
+        int sum =0;
+
+        //hiring loop -> step 1 .
+        for(int high=0;high<length;high++)
+        {
+            sum = sum + nums[high];
+            
+            //condition and firing  {static mai if}
+            while(sum>=target)
+            {
+                res = Math.min(res,high-low+1);
+                sum = sum-nums[low];
+                low++;
+            }
+
+        }
+        return (res == Integer.MAX_VALUE)? 0:res;
+    }
+}
