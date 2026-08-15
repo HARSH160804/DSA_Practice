aba04704af42092f25240fb0dcb8cba63fcb49f6
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-08-15 15:39:29 +0530

    Time: 1391 ms (12.44%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,20 +1,10 @@
-select 
-    s.student_id,
-    s.student_name,
-    sub.subject_name,
-    count(e.subject_name) as attended_exams
-
-from   Students s
-cross join   Subjects sub
-left join Examinations e
-ON s.student_id = e.student_id
-AND sub.subject_name = e.subject_name
-
-group by
-    sub.subject_name,
-    s.student_name,
-    s.student_id
-
+select a.student_id,a.student_name,b.subject_name, count(c.subject_name) as attended_exams
+from Students a
+cross join Subjects b
+left join Examinations c
+on c.student_id = a.student_id and b.subject_name =c.subject_name 
+group by a.student_name,a.student_id,b.subject_name
 order by
-    s.student_id,
-    sub.subject_name
+    a.student_id,
+    b.subject_name
+
