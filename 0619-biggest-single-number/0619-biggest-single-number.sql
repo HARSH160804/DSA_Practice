d36abfbfd7eb821120db09f1d73302256d0035d8
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-08-24 02:50:11 +0530

    Time: 482 ms (45.32%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
 SELECT MAX(num) AS num
-FROM (
+FROM MyNumbers
+WHERE num IN (
     SELECT num
     FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(num) = 1
-) AS unique_numbers;
+    HAVING COUNT(*) = 1
+);
