2e9972e21ac36946e028cf1030722ca50d2bca8c
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-09-21 16:51:24 +0530

    Time: 1 ms (100%), Space: 47 MB (86.3%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,76 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
 
-    int split = 0;
-    while (split < nums.length && nums[split] < 0) {
-    split++;
-    }
+        int[] result = new int[nums.length];
+        
+        int low=0;
+        int high =nums.length -1;
+        int k=nums.length -1;
+        while(low<=high)
+        {
+            int e1= nums[low]*nums[low];
+            int e2= nums[high]*nums[high];
 
-    
-
-    int[] a = new int[split];
-    int[] b = new int[nums.length - split];
-    int[] result = new int[nums.length];    
-
-    
-
-
-
-    // Fill a[] (negative side in reverse order + square)
-        int index = 0;
-        for (int k = split - 1; k >= 0; k--) {
-            a[index] = nums[k] * nums[k];
-            index++;
+            if(e2>=e1)
+            {
+                result[k] = e2;
+                k--;
+                high--;
+            }
+            else if(e1>e2)
+            {
+                result[k] = e1;
+                k--;
+                low++;
+            }
         }
-
-    // Fill b[] (positive side + square)
-         index = 0;
-        for (int k = split; k < nums.length; k++) {
-            b[index] = nums[k] * nums[k];
-            index++;
-        }
-
-    int i=0, j=0; 
-    
-    int n= a.length;
-    int m= b.length;
-    index =0;
-
-
-    //================== mergin of two array====================
-    while(i<n && j<m)
-    {
-        if(a[i]<b[j]){
-            result[index] = a[i];
-            i++; 
-            index++;
-        }
-        else{
-        result[index] = b[j];
-        j++; 
-        index++;}
-    }
-    while(j<m){
-        result[index] = b[j];
-        j++; 
-        index++;
-    }
-
-    while(i<n){
-        result[index] = a[i];
-            i++; 
-            index++;
-    }
-
-    //==========merging array ends================
-
-    return result;
-
-
-
-
-
-
-
+        return result;
     }
 }
