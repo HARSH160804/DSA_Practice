b8ec9f12a19eb988bba774cca885300b6a539c0d
Harsh  Amarnani <149843693+HARSH160804@users.noreply.github.com>
2026-09-21 22:11:19 +0530

    Time: 30 ms (87.11%), Space: 60.2 MB (13.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,29 @@
-class Solution 
-{
-    public List<List<Integer>> threeSum(int[] nums) 
-    {
-        List<List<Integer>> ans = new ArrayList<>();
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
         
-        int length = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for(int i = 0;i<length-2;i++)
+
+        for(int i=0;i<=nums.length-2;i++)
         {
+            //edge case of duplicate element
             if(i>0 && nums[i]==nums[i-1])
             {
                 continue;
             }
 
-            int low = i+1;
-            int high = length-1;
+            // two pointers 
+            
+            int high=nums.length-1;
+            int low=i+1;
+            
+            int target = -1*nums[i];
 
-            //two pointer start 
             while(low<high)
             {
                 int sum = nums[low]+nums[high];
-                int target = -1*nums[i];
                 if(sum==target)
                 {
                     ArrayList<Integer> temp = new ArrayList<>();
@@ -32,9 +34,8 @@ class Solution
                     low++;
                     high--;
 
-                
-
-                    while(low<high && nums[low] == nums[low-1])
+                    //edgecase
+                    while(low<high && nums[low]==nums[low-1])
                     {
                         low++;
                     }
@@ -43,37 +44,23 @@ class Solution
                     {
                         high--;
                     }
+
                 }
 
-
-                else if(sum>target)
+                if(sum>target)
                 {
                     high--;
                 }
 
-                else
+                if(sum<target)
                 {
                     low++;
                 }
-            }
-        
+            }    
+
         }
         return ans;
 
 
-
-
-
-
-        
     }
 }
-
-
-
-
-
-
-
-
-
